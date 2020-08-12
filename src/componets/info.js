1be36109943145fc99484f4af8f6036073c72216
Mikhail <leonovm143@gmail.com>
2020-08-12 21:02:02 +0600

    Added components Info.js

---
+++ src/componets/info.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Info extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Курсы валют</h2>
+                <p>Узнай актуальный курс валюты!</p>
+            </div>
+        );
+    }
+}
+
+export default Info;
